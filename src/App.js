dd2e57c832a59a00c6fc37e1036e9dace1343a6a
Nika Oniani <nikaoniani2018@gmail.com>
2023-10-29 11:16:48 +0400

    added id's to questions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import quizData from "./quizData";
+
 const App = () => {
   return (
     <div>
       <h1>Astronomy Quiz</h1>
       <p>Welcome to the astronomy quiz!</p>
+
+      {quizData.map((item) => (
+        <li key={item.id}>{item.question}</li>
+      ))}
     </div>
   );
 };
--- src/quizData.js
+++ src/quizData.js
@@ -1,29 +1,34 @@
 const quizData = [
   {
+    id: 1,
     question: "Which planet is the closes to the sun?",
     options: ["Earth", "Mercury", "Mars", "Venus"],
     answer: "Mercury",
   },
   {
+    id: 2,
     question: "Which planet is the largest in our solar system?",
     options: ["Mars", "Jupiter", "Venus", "Earth"],
     answer: "Jupiter",
   },
   {
+    id: 3,
     question: "Which celestial body is responsible for causing tides on Earth?",
     options: ["The Moon", "Saturn", "Neptune", "Pluto"],
     answer: "",
   },
   {
+    id: 4,
     question: "What is the largest moon of Jupiter?",
     options: ["Titan", "Ganymede", "Io", "Triton"],
     answer: "Ganymede",
   },
   {
+    id: 5,
     question: "Which constellation contains the North Star (Polaris)?",
     options: ["Orion", "Leo", "Scorpius", "Ursa Major"],
     answer: "Ursa Major",
   },
 ];
 
-export default quizData
+export default quizData;
