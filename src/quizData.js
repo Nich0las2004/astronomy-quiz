5ef7f7875be89c74a273f1beaf2def0baca4662e
Nika Oniani <nikaoniani2018@gmail.com>
2023-10-29 11:11:17 +0400

    added quiz questions

---
+++ src/quizData.js
@@ -0,0 +1,29 @@
+const quizData = [
+  {
+    question: "Which planet is the closes to the sun?",
+    options: ["Earth", "Mercury", "Mars", "Venus"],
+    answer: "Mercury",
+  },
+  {
+    question: "Which planet is the largest in our solar system?",
+    options: ["Mars", "Jupiter", "Venus", "Earth"],
+    answer: "Jupiter",
+  },
+  {
+    question: "Which celestial body is responsible for causing tides on Earth?",
+    options: ["The Moon", "Saturn", "Neptune", "Pluto"],
+    answer: "",
+  },
+  {
+    question: "What is the largest moon of Jupiter?",
+    options: ["Titan", "Ganymede", "Io", "Triton"],
+    answer: "Ganymede",
+  },
+  {
+    question: "Which constellation contains the North Star (Polaris)?",
+    options: ["Orion", "Leo", "Scorpius", "Ursa Major"],
+    answer: "Ursa Major",
+  },
+];
+
+export default quizData
