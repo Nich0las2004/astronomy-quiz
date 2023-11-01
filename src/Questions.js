be29f5dbbb43baefc606023820de4d0cf22fd78a
Nika Oniani <nikaoniani2018@gmail.com>
2023-11-01 22:45:49 +0400

    radio button only chooses 1 option

--- src/Questions.js
+++ src/Questions.js
@@ -1,21 +1,38 @@
+import { Fragment } from "react";
 import quizData from "./quizData";
+
+import classes from "./Questions.module.css";
 
 const Questions = () => {
   return (
-    <div>
+    <div class="quiz-container">
       {quizData.map((item) => (
-        <>
+        <div>
           <h2 key={item.id}>{item.question}</h2>
 
           {item.options.map((option) => (
             <>
-              <input type="radio" value={option} />
+              <input name="options" type="radio" value={option} />
               <label>{option}</label>
             </>
           ))}
-        </>
+        </div>
       ))}
     </div>
+    // <Fragment>
+    //   {quizData.map((item) => (
+    //     <div>
+    //       <h2 key={item.id}>{item.question}</h2>
+
+    //       {item.options.map((option) => (
+    //         <>
+    //           <input name="options" type="radio" value={option} />
+    //           <label>{option}</label>
+    //         </>
+    //       ))}
+    //     </div>
+    //   ))}
+    // </Fragment>
   );
 };
 
