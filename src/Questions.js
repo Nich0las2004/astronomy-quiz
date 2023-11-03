36e531f068371a61e2b8a4f641d25326828f3ba4
Nika Oniani <nikaoniani2018@gmail.com>
2023-11-03 19:56:30 +0400

    added submit button

--- src/Questions.js
+++ src/Questions.js
@@ -2,10 +2,11 @@ import { Fragment } from "react";
 import quizData from "./quizData";
 
 import classes from "./Questions.module.css";
+import Submit from "./Submit";
 
 const Questions = () => {
   return (
-    <div class="quiz-container">
+    <form class="quiz-container">
       {quizData.map((item) => (
         <div>
           <h2 key={item.id}>{item.question}</h2>
@@ -18,21 +19,9 @@ const Questions = () => {
           ))}
         </div>
       ))}
-    </div>
-    // <Fragment>
-    //   {quizData.map((item) => (
-    //     <div>
-    //       <h2 key={item.id}>{item.question}</h2>
 
-    //       {item.options.map((option) => (
-    //         <>
-    //           <input name="options" type="radio" value={option} />
-    //           <label>{option}</label>
-    //         </>
-    //       ))}
-    //     </div>
-    //   ))}
-    // </Fragment>
+      <Submit />
+    </form>
   );
 };
 
---
+++ src/Submit.js
@@ -0,0 +1,5 @@
+const Submit = () => {
+  return <button>Submit</button>;
+};
+
+export default Submit;
