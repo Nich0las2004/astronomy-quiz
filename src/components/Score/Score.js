2a5a51a425dfdffcb2b7c1174f4712007cb0304b
Nika Oniani <nikaoniani2018@gmail.com>
2023-11-23 01:53:30 +0400

    added dummy score

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -3,7 +3,8 @@ import classes from "./Score.module.css";
 const Score = () => {
   return (
     <span>
-      <p className={classes.score}>Score: </p> 
+      <p className={classes.score}>Score: </p>
+      <p className={classes.actualScore}>2</p>
     </span>
   );
 };
